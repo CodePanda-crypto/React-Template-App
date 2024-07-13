1c6a2ed6daa4b044f4c7a925ba4f9085a7d6c0a5
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-13 06:32:32 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-// import React from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+ import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css';
-function HelloWorld() {
-  return 'Hello World';
+
+function App() {
+  const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <ul>
+        {animals.map((animal) => {
+          return <li key={animal}>{animal}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
-export default HelloWorld;
+  export default App;
