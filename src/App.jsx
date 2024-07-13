2edf6896bbd67ec23cf0f0f116daf7711b4ddf91
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-13 10:31:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-// import React from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 function HelloWorld() {
   return 'Hello World';
