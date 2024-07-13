5d9f933a6e377b708416d266cf53e15c8860f937
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-13 10:37:51 +0500

    Up[date

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-function HelloWorld() {
-  return 'Hello World';
+
+function App() {
+  return (
+    <div className="container">
+      <svg>
+        <circle cx={25} cy={75} r={20} stroke="green" strokeWidth={2} />
+      </svg>
+    </div>
+  );
 }
-export default HelloWorld;
+export default App;
