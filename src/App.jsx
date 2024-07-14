e2b19830c3014ca4c2590fd097bc5d88c4104f67
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-14 15:38:55 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,36 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
     <div className="container">
-      <svg>
-        <circle cx={25} cy={75} r={20} stroke="green" strokeWidth={2} />
-      </svg>
+      <h1>Hello Vite + React!</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+
+      <h1>Vite + React</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   );
 }
